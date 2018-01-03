b21e81eabfbd6e49d684eec317894fc80469952b
Casey Zimmerman <caseyezimmerman@gmail.com>
2018-01-02 23:09:05 -0500

    login to database

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var config = require ('../config/config')
+var mysql = require('mysql')
+var connection = mysql.createConnection(config);
+connection.connect();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/login', (req,res,next)=>{
+	console.log(req.body)
+	console.log("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&")
+	var name = req.body.name
+	var email = req.body.email
+	var password = req.body.password
+	var insertQuery = `INSERT INTO user (name,email,password) VALUES (?,?,?)`
+	connection.query(insertQuery,[name,email,password],(error,results)=>{
+		if(error){
+			throw error
+		}else{
+			res.json({
+				name: name,
+				msg: "loginSuccess"
+			})
+		}
+	})
+})
 
 module.exports = router;
